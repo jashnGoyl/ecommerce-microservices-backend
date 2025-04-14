5384de22da1d4a11c258768509f8bc53cf1707e3
Jashan Goyal <jshnngoyal@gmail.com>
2025-04-15 01:44:51 +0530

    Created DTO classes!!!

---
+++ user-service/src/main/java/com/jashan/userservice/dto/AuthResponseDTO.java
@@ -0,0 +1,19 @@
+package com.jashan.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponseDTO {
+
+    private String token;
+
+    private String email;
+
+    private String role;
+}
---
+++ user-service/src/main/java/com/jashan/userservice/dto/RegisterRequestDTO.java
@@ -0,0 +1,20 @@
+package com.jashan.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequestDTO {
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+}
