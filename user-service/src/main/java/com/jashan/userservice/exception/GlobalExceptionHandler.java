6e91619c3bd243abe9d8cc931d2902d3cdd286b1
Jashan Goyal <jshnngoyal@gmail.com>
2025-04-23 18:44:43 +0530

    Add generic execption handler method.

--- user-service/src/main/java/com/jashan/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/jashan/userservice/exception/GlobalExceptionHandler.java
@@ -23,106 +23,121 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex,
-            HttpServletRequest httpServletRequest) {
-        List<String> errors = ex.getBindingResult().getFieldErrors()
-                .stream()
-                .map(FieldError::getDefaultMessage)
-                .toList();
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex,
+                        HttpServletRequest httpServletRequest) {
+                List<String> errors = ex.getBindingResult().getFieldErrors()
+                                .stream()
+                                .map(FieldError::getDefaultMessage)
+                                .toList();
 
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(ErrorCodeEnum.VALIDATION_ERROR.getErrorCode())
-                .errorMessage(ErrorCodeEnum.VALIDATION_ERROR.getErrorMessage())
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .httpMethod(httpServletRequest.getMethod())
-                .timestamp(LocalDateTime.now())
-                .backendMessage(ex.getMessage())
-                .details(errors)
-                .build();
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(ErrorCodeEnum.VALIDATION_ERROR.getErrorCode())
+                                .errorMessage(ErrorCodeEnum.VALIDATION_ERROR.getErrorMessage())
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .timestamp(LocalDateTime.now())
+                                .backendMessage(ex.getMessage())
+                                .details(errors)
+                                .build();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
+                return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomExceptions(CustomException ex,
-            HttpServletRequest httpServletRequest) {
+        @ExceptionHandler(CustomException.class)
+        public ResponseEntity<ErrorResponse> handleCustomExceptions(CustomException ex,
+                        HttpServletRequest httpServletRequest) {
 
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(ex.getErrorCode())
-                .errorMessage(ex.getMessage())
-                .statusCode(ex.getStatusCode())
-                .httpMethod(httpServletRequest.getMethod())
-                .timestamp(LocalDateTime.now())
-                .backendMessage(ex.getBackendMessage())
-                .details(null)
-                .build();
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(ex.getErrorCode())
+                                .errorMessage(ex.getMessage())
+                                .statusCode(ex.getStatusCode())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .timestamp(LocalDateTime.now())
+                                .backendMessage(ex.getBackendMessage())
+                                .details(null)
+                                .build();
 
-        return new ResponseEntity<>(errorResponse, ex.getStatus());
-    }
+                return new ResponseEntity<>(errorResponse, ex.getStatus());
+        }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorResponse> handleConstraintViolation(DataIntegrityViolationException ex,
-            HttpServletRequest httpServletRequest) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(DatabaseErrorCodeEnum.CONSTRAINT_VIOLATION.getErrorCode())
-                .errorMessage(DatabaseErrorCodeEnum.CONSTRAINT_VIOLATION.getErrorMessage())
-                .backendMessage(ex.getMostSpecificCause().getMessage())
-                .httpMethod(httpServletRequest.getMethod())
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .timestamp(LocalDateTime.now())
-                .build();
+        @ExceptionHandler(DataIntegrityViolationException.class)
+        public ResponseEntity<ErrorResponse> handleConstraintViolation(DataIntegrityViolationException ex,
+                        HttpServletRequest httpServletRequest) {
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(DatabaseErrorCodeEnum.CONSTRAINT_VIOLATION.getErrorCode())
+                                .errorMessage(DatabaseErrorCodeEnum.CONSTRAINT_VIOLATION.getErrorMessage())
+                                .backendMessage(ex.getMostSpecificCause().getMessage())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .timestamp(LocalDateTime.now())
+                                .build();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
+                return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-    @ExceptionHandler(CannotCreateTransactionException.class)
-    public ResponseEntity<ErrorResponse> handleConnectionFailure(CannotCreateTransactionException ex,
-            HttpServletRequest httpServletRequest) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(DatabaseErrorCodeEnum.CONNECTION_FAILURE.getErrorCode())
-                .errorMessage(DatabaseErrorCodeEnum.CONNECTION_FAILURE.getErrorMessage())
-                .backendMessage(ex.getMessage())
-                .httpMethod(httpServletRequest.getMethod())
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .timestamp(LocalDateTime.now())
-                .build();
+        @ExceptionHandler(CannotCreateTransactionException.class)
+        public ResponseEntity<ErrorResponse> handleConnectionFailure(CannotCreateTransactionException ex,
+                        HttpServletRequest httpServletRequest) {
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(DatabaseErrorCodeEnum.CONNECTION_FAILURE.getErrorCode())
+                                .errorMessage(DatabaseErrorCodeEnum.CONNECTION_FAILURE.getErrorMessage())
+                                .backendMessage(ex.getMessage())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                                .timestamp(LocalDateTime.now())
+                                .build();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+                return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedExceptions(
-            HttpRequestMethodNotSupportedException ex,
-            HttpServletRequest httpServletRequest) {
+        @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+        public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedExceptions(
+                        HttpRequestMethodNotSupportedException ex,
+                        HttpServletRequest httpServletRequest) {
 
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(ErrorCodeEnum.GENERIC_ERROR.getErrorCode())
-                .errorMessage("HTTP method not supported!!")
-                .statusCode(HttpStatus.METHOD_NOT_ALLOWED.value())
-                .httpMethod(httpServletRequest.getMethod())
-                .timestamp(LocalDateTime.now())
-                .backendMessage(ex.getMessage())
-                .details(null)
-                .build();
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(ErrorCodeEnum.GENERIC_ERROR.getErrorCode())
+                                .errorMessage("HTTP method not supported!!")
+                                .statusCode(HttpStatus.METHOD_NOT_ALLOWED.value())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .timestamp(LocalDateTime.now())
+                                .backendMessage(ex.getMessage())
+                                .details(null)
+                                .build();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
-    }
+                return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
+        }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex,
-            HttpServletRequest httpServletRequest) {
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .errorCode(ErrorCodeEnum.GENERIC_ERROR.getErrorCode())
-                .errorMessage("Invalid request body")
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .httpMethod(httpServletRequest.getMethod())
-                .backendMessage(ex.getMessage())
-                .timestamp(LocalDateTime.now())
-                .details(null)
-                .build();
+        @ExceptionHandler(HttpMessageNotReadableException.class)
+        public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex,
+                        HttpServletRequest httpServletRequest) {
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(ErrorCodeEnum.GENERIC_ERROR.getErrorCode())
+                                .errorMessage("Invalid request body")
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .httpMethod(httpServletRequest.getMethod())
+                                .backendMessage(ex.getMessage())
+                                .timestamp(LocalDateTime.now())
+                                .details(null)
+                                .build();
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, HttpServletRequest request) {
+                ErrorResponse errorResponse = ErrorResponse.builder()
+                                .errorCode(ErrorCodeEnum.GENERIC_ERROR.getErrorCode())
+                                .errorMessage(ErrorCodeEnum.GENERIC_ERROR.getErrorMessage())
+                                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                                .httpMethod(request.getMethod())
+                                .timestamp(LocalDateTime.now())
+                                .backendMessage(ex.getMessage())
+                                .details(null)
+                                .build();
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
 
 }
